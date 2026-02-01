52dc59d434b86b6d664b4c05776a4aaadb9325a3
Davi Lisboa <davifranca135@gmail.com>
2026-02-01 00:02:41 -0300

    Update HelloApplication.java

--- src/main/java/com/example/miniwebbrowserjavafx/HelloApplication.java
+++ src/main/java/com/example/miniwebbrowserjavafx/HelloApplication.java
@@ -14,7 +14,27 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        TextField UrlField = new TextField();
+        WebView browser = new WebView();
+        WebEngine engine = browser.getEngine();
 
+        // Load a web page when the user press 'enter'
+        UrlField.setOnAction(event -> engine.load( UrlFormatter(UrlField.getText()) ) );
+
+        VBox vbox = new VBox();
+        vbox.getChildren().addAll(UrlField, browser);
+        Scene scene = new Scene(vbox);
+
+        stage.setTitle("My Java Browser");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public String UrlFormatter (String url) {
+        if (!url.startsWith("http://") && !url.startsWith("https://")) {
+            url = "http://" + url;
+        }
+        return url;
     }
 
     public static void main(String[] args) {
